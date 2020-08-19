c29aa22931393329f6a77c76b72d15f0ef063cdf
Anshegar <mtxvoron@mail.ru>
2020-08-19 15:12:56 +0400

    add some comments

--- api_nkp.py
+++ api_nkp.py
@@ -6,6 +6,7 @@ from urllib.parse import urlparse
 from proxy.const import API_BOT_NAME, API_BOT_PASS, URL
 
 # API classic
+# Вместо '...' вписываем адрес API1
 def api_c():
     data1 = requests.get('...')
     print(data1.content)
@@ -15,6 +16,7 @@ def api_c():
 
 
 #API flask
+# Вместо '...' вписываем адрес API2
 def api_f():
     data = requests.get('...')
     soup = BeautifulSoup(data.text, 'html.parser')
@@ -22,6 +24,7 @@ def api_f():
     return f'{soup}'
 
 # Авторизация
+# Тут идет забор данных по API URL с вводом имени бота и его пароля на сайте  
 def api_f_auth():
     data = requests.get(URL+API_BOT_NAME+API_BOT_PASS)
     soup = BeautifulSoup(data.text, 'html.parser')
